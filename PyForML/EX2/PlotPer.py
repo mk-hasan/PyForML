04c36cf86576110a1b0bc0760b36b922caf0ab76
mk-hasan <mk-hasan@campus.tu-berlin.de>
2017-11-12 17:36:07 +0100

    #testing the performance of pybatch and new npbatch

--- PyForML/EX2/PlotPer.py
+++ PyForML/EX2/PlotPer.py
@@ -79,3 +79,24 @@ plt.plot(dlist2,tlist1,'-o',label='npdistance')
 plt.plot(dlist2,tlist2,'-o',label='npnearest')
 plt.legend(loc='lower left')
 plt.xscale('log');plt.yscale('log');plt.xlabel('d');plt.ylabel('time');plt.grid(True)
+
+#testing the performance of pybatch and new npbatch
+
+Mlist = [1, 2, 5, 10, 20, 50, 100, 200, 500, 1000]
+tlist1 = []
+tlist2 = []
+for M in Mlist:
+    U,X,Y = data.toy(100,M,100)
+    a = time.clock()
+    pybatch(U,X,Y,distance=npdistance)
+    b = time.clock()
+    npbatch(U,X,Y)
+    c = time.clock()
+    tlist1 += [b-a]
+    tlist2 += [c-b]
+# Plot the results in a graph
+plt.figure(figsize=(5,3))
+plt.plot(Mlist,tlist1,'-o')
+plt.plot(Mlist,tlist2,'-o')
+plt.xscale('log');plt.yscale('log'); plt.xlabel('M'); plt.ylabel('time'); plt.grid(True
+)
