3ea4d6aeeca827d6e60b1531bfbfb07792c545c7
mk-hasan <mk-hasan@campus.tu-berlin.de>
2017-11-12 16:31:21 +0100

    #Testing the performance of pybatch with pydistance and npdistance method

--- PyForML/EX2/PlotPer.py
+++ PyForML/EX2/PlotPer.py
@@ -24,3 +24,31 @@ for d in dlist:
 plt.figure(figsize=(5,3))
 plt.plot(dlist,tlist,'-o')
 plt.xscale('log');plt.yscale('log');plt.xlabel('d');plt.ylabel('time');plt.grid(True)
+
+
+
+#Testing the performance of pybatch with pydistance and npdistance method
+
+dlist2=[1,2,5,10,20,50,100,200,500,1000]
+
+tlist1 =[]
+tlist2=[]
+
+for d in dlist2:
+    U,X,Y = data.toy(100,100,d)
+    a = time.clock()
+    pybatch(U,X,Y)
+    b=time.clock()
+    pybatch(U,X,Y,distance=npdistance)
+    
+    c=time.clock()
+    tlist1+=[b-a]
+    tlist2+=[c-b]
+    
+#plot the result in a graph
+
+plt.figure(figsize=(5,3))
+plt.plot(dlist2,tlist1,'-o',label='normal')
+plt.plot(dlist2,tlist2,'-o',label='npdistance')
+plt.legend(loc='upper left')
+plt.xscale('log');plt.yscale('log');plt.xlabel('d');plt.ylabel('time');plt.grid(True)
