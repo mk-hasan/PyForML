c96311d693f3809fa8fef01afeef252efb7731a6
mk-hasan <mk-hasan@campus.tu-berlin.de>
2017-11-12 16:27:09 +0100

    new distance method with numpy for avoiding for loop

--- PyForML/EX2/NNAWitNumPy.py
+++ PyForML/EX2/NNAWitNumPy.py
@@ -1,5 +1,6 @@
 import data 
 
+import numpy as np
 
 def pydistance(x1,x2):
     return sum([(x1d-x2d)**2 for x1d,x2d in zip(x1,x2)])
@@ -24,3 +25,18 @@ def pybatch(U,X,Y,nearest=pynearest,distance=pydistance):
 
 U,X,Y = data.toy(20,100,50)
 print (pybatch(U,X,Y))
+
+#making new distance with numpy instead of loop
+
+def npdistance(x1,x2):
+    x1=np.array(x1)
+    x2=np.array(x2)
+    return np.sum((x1-x2)**2)
+
+U,X,Y = data.toy(20,100,50)
+
+batch_num = pybatch(U,X,Y,distance=npdistance)
+batch_py = pybatch(U,X,Y)
+print "Result are equal??"
+
+print batch_num == batch_py
